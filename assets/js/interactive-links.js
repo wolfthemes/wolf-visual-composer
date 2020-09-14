966081498a7a1df6f40a163239afc28c60c1d8ac
WolfThemes <contact@wolfthemes.com>
2020-09-14 12:42:32 +0200

    Fixed interactive links hover

--- assets/js/interactive-links.js
+++ assets/js/interactive-links.js
@@ -6,16 +6,14 @@
 /* jshint -W062 */
 
 /* global WVC, WVCParams, WVCYTVideoBg, WVCVimeo, Vimeo */
-var WVCInteractiveLinks = function( $ ) {
-
-	'use strict';
+var WVCInteractiveLinks = (function ($) {
+	"use strict";
 
 	return {
-
 		/**
 		 * Init UI
 		 */
-		init : function () {
+		init: function () {
 			this.build();
 			this.interaction();
 		},
@@ -23,212 +21,227 @@ var WVCInteractiveLinks = function( $ ) {
 		/**
 		 * Build markup
 		 */
-		build : function() {
-
+		build: function () {
 			var _this = this;
 
-			$( '.wvc-interactive-links' ).each( function() {
+			$(".wvc-interactive-links").each(function () {
+				var $container = $(this),
+					$holder = $container.find(
+						".wvc-interactive-links-bg-holder"
+					);
 
-				var $container = $( this ),
-					$holder = $container.find( '.wvc-interactive-links-bg-holder' );
+				$container.closest(".wvc-column").addClass("wvc-column-has-il");
+				$container
+					.find(".wvc-interactive-link-item:first-child")
+					.addClass("wvc-ils-active");
 
-				$container.closest( '.wvc-column' ).addClass( 'wvc-column-has-il' );
-				$container.find( '.wvc-interactive-link-item:first-child' ).addClass( 'wvc-ils-active' );
+				$container
+					.find(".wvc-interactive-link-item")
+					.each(function (index) {
+						var $this = $(this),
+							src = $this.data("bg-src"),
+							data = $this.data("bg-atts"),
+							bgType = data["type"],
+							$bgContent;
 
-				$container.find( '.wvc-interactive-link-item' ).each( function( index ) {
-					
-					var $this =  $( this ),
-						src = $this.data( 'bg-src' ),
-						data = $this.data( 'bg-atts' ),
-						bgType = data['type'],
-						$bgContent;
+						data["action"] = "wvc_ajax_get_bg_markup";
 
-					data['action'] = 'wvc_ajax_get_bg_markup';
-
-					$holder
-						.append( '<div class="wvc-ils-bg" data-bg-index="' + index + '" />' );
+						$holder.append(
+							'<div class="wvc-ils-bg" data-bg-index="' +
+								index +
+								'" />'
+						);
 						//.append( '<div class="wvc-ils-bg" data-bg-index="' + index + '" style="background-image:url(' + src + ')" />' );
 
-					$bgContent = $( '[data-bg-index="' + index + '"]' );
+						$bgContent = $('[data-bg-index="' + index + '"]');
 
-					$.post( WVCParams.ajaxUrl, data, function( response ) {
+						$.post(WVCParams.ajaxUrl, data, function (response) {
+							if (response) {
+								$bgContent.append(response);
 
-						if ( response ) {
-													
-							$bgContent.append( response );
+								_this.initVideoBg($bgContent);
+								window.dispatchEvent(new Event("resize"));
 
-							_this.initVideoBg( $bgContent );
-							window.dispatchEvent( new Event( 'resize' ) );
-
-							if ( 0 === index ) {
-								
-								$bgContent.addClass( 'wvc-ils-bg-active' );
-							
-							} else if ( $bgContent.length === index ) {
-
-								$holder.removeClass( 'wvc-il-bg-holder-loading' );
-								//console.log( 'loaded' );
+								if (0 === index) {
+									$bgContent.addClass("wvc-ils-bg-active");
+								} else if ($bgContent.length === index) {
+									$holder.removeClass(
+										"wvc-il-bg-holder-loading"
+									);
+									//console.log( 'loaded' );
+								}
+							} else {
+								console.log("AJAX error");
 							}
-
-						} else {
-							console.log( 'AJAX error' );
-						}
-					} );
-				} );
-			} );
+						});
+					});
+			});
 		},
 
 		/**
 		 * Interaction on mouse hover
 		 */
-		interaction : function () {
-
+		interaction: function () {
 			var _this = this;
 
-			$( document ).on( 'hover', '.wvc-interactive-link-item a', function() {
+			$(document).on(
+				"mouseover",
+				".wvc-interactive-link-item a",
+				function () {
+					var $link = $(this),
+						$item = $link.parent(),
+						item = document.getElementById($item.attr("id")),
+						$container = $item.closest(
+							".wvc-interactive-links-container"
+						),
+						index = $container
+							.find(".wvc-interactive-link-item")
+							.index(item);
 
-				var $link = $( this ),
-					$item = $link.parent(),
-					item = document.getElementById( $item.attr( 'id' ) ),
-					$container = $item.closest( '.wvc-interactive-links-container' ),
-					index = $container.find( '.wvc-interactive-link-item' ).index( item );
+					$container.addClass("wvc-loading");
 
-				$container.addClass( 'wvc-loading' );
+					if ($item.hasClass(".wvc-ils-active")) {
+						return;
+					}
 
-				if ( $item.hasClass( '.wvc-ils-active' ) ) {
-					return;
+					$container
+						.find(".wvc-interactive-link-item")
+						.removeClass("wvc-ils-active");
+					$item.addClass("wvc-ils-active");
+
+					_this.pauseAllVideos($(".wvc-interactive-links"));
+
+					$container
+						.find(".wvc-ils-bg")
+						.removeClass("wvc-ils-bg-active");
+					$container
+						.find(".wvc-ils-bg")
+						.eq(index)
+						.addClass("wvc-ils-bg-active");
+
+					_this.startCurrentVideo($(".wvc-ils-bg-active"));
+
+					setTimeout(function () {
+						$container.removeClass("wvc-loading");
+					}, 500);
 				}
-
-				$container.find( '.wvc-interactive-link-item' ).removeClass( 'wvc-ils-active' );
-				$item.addClass( 'wvc-ils-active' );
-
-				_this.pauseAllVideos( $( '.wvc-interactive-links' ) );
-
-				$container.find( '.wvc-ils-bg' ).removeClass( 'wvc-ils-bg-active' );
-				$container.find( '.wvc-ils-bg' ).eq( index ).addClass( 'wvc-ils-bg-active' );
-
-				_this.startCurrentVideo( $( '.wvc-ils-bg-active' ) );
-
-				setTimeout( function() {
-					$container.removeClass( 'wvc-loading' );
-				}, 500 );
-			} );
+			);
 		},
 
 		/**
 		 * Init & Stop video bg on first load
 		 */
-		initVideoBg : function( $container ) {
-
-			var $iframe = $container.find( 'iframe' ),
-				$video = $container.find( 'video' ),
+		initVideoBg: function ($container) {
+			var $iframe = $container.find("iframe"),
+				$video = $container.find("video"),
 				YTPlayerId,
 				VimeoPlayerId,
 				timeOut = 2000;
 
 			/* YT */
-			if ( $container.find( '.wvc-youtube-video-bg-container' ).length ) {
+			if ($container.find(".wvc-youtube-video-bg-container").length) {
+				YTPlayerId = $container
+					.find(".wvc-youtube-video-bg-container")
+					.data("yt-bg-element-id");
 
-				YTPlayerId = $container.find( '.wvc-youtube-video-bg-container' ).data( 'yt-bg-element-id' );
+				WVCYTVideoBg.init($container);
 
-				WVCYTVideoBg.init( $container );
-
-				setTimeout( function() {
+				setTimeout(function () {
 					WVCYTVideoBg.players[YTPlayerId].pause();
-				}, timeOut );
+				}, timeOut);
 
-			/* Vimeo */
-			} else if ( $iframe.length && $iframe.hasClass( 'wvc-vimeo-bg' ) ) {
-				
-				VimeoPlayerId = $( this ).data( 'vimeo-bg-element-id' );
+				/* Vimeo */
+			} else if ($iframe.length && $iframe.hasClass("wvc-vimeo-bg")) {
+				VimeoPlayerId = $(this).data("vimeo-bg-element-id");
 
-				WVCVimeo.init( $container );
+				WVCVimeo.init($container);
 
-				setTimeout( function() {
-					if ( 'undefined' !== typeof WVCVimeo && WVCVimeo.players[ VimeoPlayerId ] ) {
-						WVCVimeo.players[ VimeoPlayerId ].pause();
+				setTimeout(function () {
+					if (
+						"undefined" !== typeof WVCVimeo &&
+						WVCVimeo.players[VimeoPlayerId]
+					) {
+						WVCVimeo.players[VimeoPlayerId].pause();
 					}
-				}, timeOut );
+				}, timeOut);
 
-			/* Self Hosted */
-			} else if ( $video.length ) {
-
-				setTimeout( function() {
-					$video.trigger( 'play' );
-				}, timeOut );
+				/* Self Hosted */
+			} else if ($video.length) {
+				setTimeout(function () {
+					$video.trigger("play");
+				}, timeOut);
 			}
 		},
 
 		/**
 		 * Start active video background
 		 */
-		startCurrentVideo : function( $container ) {
-			
-			var $iframe = $container.find( 'iframe' ),
-				YTPlayerId = $iframe.parent().data( 'yt-bg-element-id' ),
-				VimeoPlayerId = $iframe.data( 'vimeo-bg-element-id' ),
-				$video = $container.find( 'video' );
+		startCurrentVideo: function ($container) {
+			var $iframe = $container.find("iframe"),
+				YTPlayerId = $iframe.parent().data("yt-bg-element-id"),
+				VimeoPlayerId = $iframe.data("vimeo-bg-element-id"),
+				$video = $container.find("video");
 
-			if ( $iframe.length ) {
-
-				if ( $iframe.hasClass( 'wvc-youtube-bg' ) ) {
-
-					if ( 'undefined' !== typeof WVCYTVideoBg && WVCYTVideoBg.players[ YTPlayerId ] ) {
+			if ($iframe.length) {
+				if ($iframe.hasClass("wvc-youtube-bg")) {
+					if (
+						"undefined" !== typeof WVCYTVideoBg &&
+						WVCYTVideoBg.players[YTPlayerId]
+					) {
 						WVCYTVideoBg.players[YTPlayerId].playVideo();
 					}
-					
-				} else if ( $iframe.hasClass( 'wvc-vimeo-bg' ) ) {
-
-					if ( 'undefined' !== typeof WVCVimeo && WVCVimeo.players[ VimeoPlayerId ] ) {
-						WVCVimeo.players[ VimeoPlayerId ].play();
+				} else if ($iframe.hasClass("wvc-vimeo-bg")) {
+					if (
+						"undefined" !== typeof WVCVimeo &&
+						WVCVimeo.players[VimeoPlayerId]
+					) {
+						WVCVimeo.players[VimeoPlayerId].play();
 					}
 				}
-
-			} else if ( $video.length ) {
-
-				$video.trigger( 'play' );
+			} else if ($video.length) {
+				$video.trigger("play");
 			}
-
 		},
 
 		/**
 		 * Pause all videos
 		 */
-		pauseAllVideos : function ( $container ) {
-
+		pauseAllVideos: function ($container) {
 			var _this = this,
 				VimeoPlayerId,
 				$video;
 
 			/* Pause all HTML videos */
-			if ( $container.find( '.wvc-video-bg' ).length ) {
-				$container.find( '.wvc-video-bg' ).each( function () {
-					$video = $( this );
+			if ($container.find(".wvc-video-bg").length) {
+				$container.find(".wvc-video-bg").each(function () {
+					$video = $(this);
 					$video.get(0).pause();
-				} );
+				});
 			}
 
 			/* Pause all YT videos */
-			$container.find( '.wvc-yt-video-bg-pause' ).trigger( 'click' );
+			$container.find(".wvc-yt-video-bg-pause").trigger("click");
 
 			/* Pause all Vimeo  videos */
-			$container.find( '.wvc-vimeo-video-bg-container > iframe' ).each( function () {
-				VimeoPlayerId = $( this ).data( 'vimeo-bg-element-id' );
-				if ( 'undefined' !== typeof WVCVimeo && WVCVimeo.players[ VimeoPlayerId ] ) {
-					WVCVimeo.players[ VimeoPlayerId ].pause();
-				}
-			} );
-		}
+			$container
+				.find(".wvc-vimeo-video-bg-container > iframe")
+				.each(function () {
+					VimeoPlayerId = $(this).data("vimeo-bg-element-id");
+					if (
+						"undefined" !== typeof WVCVimeo &&
+						WVCVimeo.players[VimeoPlayerId]
+					) {
+						WVCVimeo.players[VimeoPlayerId].pause();
+					}
+				});
+		},
 	};
+})(jQuery);
 
-}( jQuery );
+(function ($) {
+	"use strict";
 
-( function( $ ) {
-
-	'use strict';
-
-	$( document ).ready( function() {
+	$(document).ready(function () {
 		WVCInteractiveLinks.init();
-	} );
-
-} )( jQuery );
+	});
+})(jQuery);
--- assets/js/interactive-overlays.js
+++ assets/js/interactive-overlays.js
@@ -6,17 +6,14 @@
 /* jshint -W062 */
 
 /* global WVC, WVCParams, WOW, Vimeo, $f */
-var WVCInteractiveOverlays = function( $ ) {
-
-	'use strict';
+var WVCInteractiveOverlays = (function ($) {
+	"use strict";
 
 	return {
-
 		/**
 		 * Init UI
 		 */
-		init : function () {
-
+		init: function () {
 			this.setOverlays();
 			this.interaction();
 		},
@@ -24,183 +21,216 @@ var WVCInteractiveOverlays = function( $ ) {
 		/**
 		 * Built overlays structure
 		 */
-		setOverlays : function () {
-			
-			$( '.wvc-interactive-overlays' ).each( function() {
+		setOverlays: function () {
+			$(".wvc-interactive-overlays").each(function () {
+				var $container = $(this);
 
-				var $container = $( this );
+				$container.closest(".wvc-column").addClass("wvc-column-has-io");
+				$container
+					.find(".wvc-interactive-overlay-item:first-child")
+					.addClass("wvc-io-active");
 
-				$container.closest( '.wvc-column' ).addClass( 'wvc-column-has-io' );
-				$container.find( '.wvc-interactive-overlay-item:first-child' ).addClass( 'wvc-io-active' );
+				$container
+					.find(".wvc-interactive-overlay-item")
+					.each(function (index) {
+						var src = $(this).data("bg-src");
 
-				$container.find( '.wvc-interactive-overlay-item' ).each( function( index ) {
-					var src = $( this ).data( 'bg-src' );
+						$(this).data("index", index);
 
-					$( this ).data( 'index', index );
+						$container
+							.find(".wvc-interactive-overlays-image-holder")
+							.append(
+								'<div class="wvc-io-bg" data-bg-index="' +
+									index +
+									'" style="background-image:url(' +
+									src +
+									')" />'
+							);
 
-					$container.find( '.wvc-interactive-overlays-image-holder' )
-					.append( '<div class="wvc-io-bg" data-bg-index="' + index + '" style="background-image:url(' + src + ')" />' );
-				
-					$( this ).find( '.wvc-io-item-content' ).attr( 'id', 'wvc-io-item-content-' + index ).data( 'index', index ).prepend( '<span class="wvc-io-item-content-close">&times;</span>' ).detach().appendTo( 'body' );
-				
-				} );
+						$(this)
+							.find(".wvc-io-item-content")
+							.attr("id", "wvc-io-item-content-" + index)
+							.data("index", index)
+							.prepend(
+								'<span class="wvc-io-item-content-close">&times;</span>'
+							)
+							.detach()
+							.appendTo("body");
+					});
 
-				$container.find( '.wvc-interactive-overlays-image-holder .wvc-io-bg:first-child' ).addClass( 'wvc-io-bg-active' );
-			
-				$container.find( '.wvc-io-item-link' ).each( function( index ) {
+				$container
+					.find(
+						".wvc-interactive-overlays-image-holder .wvc-io-bg:first-child"
+					)
+					.addClass("wvc-io-bg-active");
 
-					var $content = $( '#wvc-io-item-content-' + index );
+				$container.find(".wvc-io-item-link").each(function (index) {
+					var $content = $("#wvc-io-item-content-" + index);
 
-					if ( $content.data( 'revslider-id' ) ) {
-						$( this ).attr( 'data-revslider-id', $content.data( 'revslider-id' ) );
+					if ($content.data("revslider-id")) {
+						$(this).attr(
+							"data-revslider-id",
+							$content.data("revslider-id")
+						);
 					}
-				} );
-			} );
+				});
+			});
 		},
 
 		/**
 		 * Interaction on mouse hover
 		 */
-		interaction : function () {
-
+		interaction: function () {
 			var _this = this;
 
-			$( document ).on( 'hover', '.wvc-io-item-link', function() {
-
-				var $overlay = $( this ),
+			$(document).on("mouseover", ".wvc-io-item-link", function () {
+				var $overlay = $(this),
 					$item = $overlay.parent(),
-					item = document.getElementById( $item.attr( 'id' ) ),
-					$container = $item.closest( '.wvc-interactive-overlays-container' ),
-					index = $container.find( '.wvc-interactive-overlay-item' ).index( item );
+					item = document.getElementById($item.attr("id")),
+					$container = $item.closest(
+						".wvc-interactive-overlays-container"
+					),
+					index = $container
+						.find(".wvc-interactive-overlay-item")
+						.index(item);
 
-				if ( $item.hasClass( '.wvc-io-active' ) ) {
+				if ($item.hasClass(".wvc-io-active")) {
 					return;
 				}
 
-				$container.find( '.wvc-interactive-overlay-item' ).removeClass( 'wvc-io-active' );
-				$item.addClass( 'wvc-io-active' );
+				$container
+					.find(".wvc-interactive-overlay-item")
+					.removeClass("wvc-io-active");
+				$item.addClass("wvc-io-active");
 
-				$container.find( '.wvc-io-bg' ).removeClass( 'wvc-io-bg-active' );
-				$container.find( '.wvc-io-bg' ).eq( index ).addClass( 'wvc-io-bg-active' );
-			} );
+				$container.find(".wvc-io-bg").removeClass("wvc-io-bg-active");
+				$container
+					.find(".wvc-io-bg")
+					.eq(index)
+					.addClass("wvc-io-bg-active");
+			});
 
-			$( document ).on( 'click', '.wvc-io-item-link', function( event ) {
+			$(document).on("click", ".wvc-io-item-link", function (event) {
 				event.preventDefault();
 				event.stopPropagation();
 
-				var index = $( this ).parent().data( 'index' ),
-					$content = $( '#wvc-io-item-content-' + index );
+				var index = $(this).parent().data("index"),
+					$content = $("#wvc-io-item-content-" + index);
 
-				$( '.wvc-io-item-content' ).removeClass( 'wvc-io-content-active' );
-				$content.addClass( 'wvc-io-content-active' );
-				
+				$(".wvc-io-item-content").removeClass("wvc-io-content-active");
+				$content.addClass("wvc-io-content-active");
+
 				// do wow
-				$content.one( WVC.transitionEventEnd(), function() {
-					WVC.doWow( index );
+				$content.one(WVC.transitionEventEnd(), function () {
+					WVC.doWow(index);
 					WVC.doAOS();
-				} );
+				});
 
 				// Start or resume revslider if any
-				if ( $( this ).data( 'revslider-id' ) ) {
-					
-					if (  ! $( this ).hasClass( 'wvc-io-link-revslider-started' ) ) {
-						
-						window['revapi' + $( this ).data( 'revslider-id' )].revstart();
-						$( '.wvc-io-item-link' ).addClass( 'wvc-io-link-revslider-started' );
+				if ($(this).data("revslider-id")) {
+					if (!$(this).hasClass("wvc-io-link-revslider-started")) {
+						window[
+							"revapi" + $(this).data("revslider-id")
+						].revstart();
+						$(".wvc-io-item-link").addClass(
+							"wvc-io-link-revslider-started"
+						);
 
-						console.log( 'start' );
+						console.log("start");
 
 						// Force video play
-						_this.startCurrentVideo( $( '.active-revslide' ) );
-					
+						_this.startCurrentVideo($(".active-revslide"));
 					} else {
-
-						window['revapi' + $( this ).data( 'revslider-id' )].revredraw();
+						window[
+							"revapi" + $(this).data("revslider-id")
+						].revredraw();
 						//window['revapi' + $( this ).data( 'revslider-id' )].revresume();
 						//window['revapi' + $( this ).data( 'revslider-id' )].revshowslide( 1 );
 
-						console.log( 'redraw' );
+						console.log("redraw");
 					}
 
 					//_this.setSlideIdOnSlideChange( $( this ).data( 'revslider-id' ) );
 				}
-			} );
+			});
 
-			$( document ).on( 'click', '.wvc-io-item-content-close', function( event ) {
+			$(document).on("click", ".wvc-io-item-content-close", function (
+				event
+			) {
 				event.preventDefault();
 				event.stopPropagation();
 
-				var $closeButton = $( this );
+				var $closeButton = $(this);
 
-				$( '.wvc-io-item-content' ).removeClass( 'wvc-io-content-active' );
-				$( 'body' ).removeClass( 'wvc-io-open' );
+				$(".wvc-io-item-content").removeClass("wvc-io-content-active");
+				$("body").removeClass("wvc-io-open");
 
-				$( this ).parent().one( WVC.transitionEventEnd(), function() {
-					$( '.wvc-io-item-content' ).find( '.wvc-delayed-wow' ).css( {
-						'visibility' : 'hidden'
-					} );
+				$(this)
+					.parent()
+					.one(WVC.transitionEventEnd(), function () {
+						$(".wvc-io-item-content").find(".wvc-delayed-wow").css({
+							visibility: "hidden",
+						});
 
-					// Stop revslider if any
-					if ( $closeButton.parent().data( 'revslider-id' ) ) {
-						WVC.pausePlayers();
-						_this.pauseRevSlider( $closeButton.parent().data( 'revslider-id' ) );
-					}
+						// Stop revslider if any
+						if ($closeButton.parent().data("revslider-id")) {
+							WVC.pausePlayers();
+							_this.pauseRevSlider(
+								$closeButton.parent().data("revslider-id")
+							);
+						}
 
-					// Reset AOS animation
-					WVC.resetAOS( '.wvc-io-item-content' );
-				} );
-			} );
+						// Reset AOS animation
+						WVC.resetAOS(".wvc-io-item-content");
+					});
+			});
 		},
 
 		/**
 		 * Stop revslider and videos in it when closing overlay
 		 */
-		pauseRevSlider : function ( revSliderId ) {
-
+		pauseRevSlider: function (revSliderId) {
 			// Pause video
-			if ( $( '.active-revslide' ).find( '.tp-videolayer' ) ) {
-				
-				var $videoContainer = $( '.active-revslide' ).find( '.tp-videolayer' );
+			if ($(".active-revslide").find(".tp-videolayer")) {
+				var $videoContainer = $(".active-revslide").find(
+					".tp-videolayer"
+				);
 
 				// case YT
-				if ( $videoContainer.data( 'ytid' ) ) {
+				if ($videoContainer.data("ytid")) {
+					$videoContainer.data("player").pauseVideo();
 
-					$videoContainer.data( 'player' ).pauseVideo();
+					// case Vimeo
+				} else if ($videoContainer.data("vimeoid")) {
+					var iframe = $videoContainer.find("iframe"),
+						player = $f(iframe[0]);
 
-				// case Vimeo
-				} else if ( $videoContainer.data( 'vimeoid' ) ) {
+					player.api("pause");
 
-					var iframe = $videoContainer.find( 'iframe' ),
-						player = $f( iframe[0] );
-					
-					player.api( 'pause' );
-
-				// case HTML5
-				} else if ( $videoContainer.find( '> .html5vid' ).length ) {
-					
-					$videoContainer.find( '> .html5vid > video' ).get(0).pause();
-				} 
+					// case HTML5
+				} else if ($videoContainer.find("> .html5vid").length) {
+					$videoContainer.find("> .html5vid > video").get(0).pause();
+				}
 			}
 
 			// Pause Player
-			window['revapi' + revSliderId].revpause();
+			window["revapi" + revSliderId].revpause();
 		},
 
 		/**
 		 * Stop an iframe or HTML5 <video> from playing
 		 * @param  {Element} element The element that contains the video
 		 */
-		stopVideo : function ( element ) {
-			
-			var iframe = element.querySelector( 'iframe'),
-				video = element.querySelector( 'video' );
-			
-			if ( iframe ) {
+		stopVideo: function (element) {
+			var iframe = element.querySelector("iframe"),
+				video = element.querySelector("video");
+
+			if (iframe) {
 				var iframeSrc = iframe.src;
 				iframe.src = iframeSrc;
 			}
 
-			if ( video ) {
+			if (video) {
 				video.pause();
 			}
 		},
@@ -208,10 +238,17 @@ var WVCInteractiveOverlays = function( $ ) {
 		/**
 		 * Set current slide on slide change
 		 */
-		setSlideIdOnSlideChange : function ( revSliderId ) {
-			window['revapi' + revSliderId].bind( 'revolution.slide.onafterswap', function ( e, data ) {
-				$( '.wvc-io-item-link[data-revslider-id="' + revSliderId + '"]' ).attr( 'data-current-revslide-id', data.slideIndex );
-			} );
+		setSlideIdOnSlideChange: function (revSliderId) {
+			window["revapi" + revSliderId].bind(
+				"revolution.slide.onafterswap",
+				function (e, data) {
+					$(
+						'.wvc-io-item-link[data-revslider-id="' +
+							revSliderId +
+							'"]'
+					).attr("data-current-revslide-id", data.slideIndex);
+				}
+			);
 		},
 
 		/**
@@ -219,88 +256,86 @@ var WVCInteractiveOverlays = function( $ ) {
 		 *
 		 * disabled
 		 */
-		startCurrentVideo : function( $container ) {
-			
-			var $iframe = $container.find( 'iframe' ),
-				YTPlayerId = $iframe.parent().data( 'yt-bg-element-id' ),
-				VimeoPlayerId = $iframe.data( 'vimeo-bg-element-id' ),
-				$video = $container.find( 'video' );
+		startCurrentVideo: function ($container) {
+			var $iframe = $container.find("iframe"),
+				YTPlayerId = $iframe.parent().data("yt-bg-element-id"),
+				VimeoPlayerId = $iframe.data("vimeo-bg-element-id"),
+				$video = $container.find("video");
 
-			if ( $iframe.length ) {
-
-				if ( $iframe.hasClass( 'wvc-youtube-bg' ) ) {
-
-					if ( 'undefined' !== typeof WVCYTVideoBg && WVCYTVideoBg.players[ YTPlayerId ] ) {
+			if ($iframe.length) {
+				if ($iframe.hasClass("wvc-youtube-bg")) {
+					if (
+						"undefined" !== typeof WVCYTVideoBg &&
+						WVCYTVideoBg.players[YTPlayerId]
+					) {
 						WVCYTVideoBg.players[YTPlayerId].playVideo();
 					}
-					
-				} else if ( $iframe.hasClass( 'wvc-vimeo-bg' ) ) {
-
-					if ( 'undefined' !== typeof WVCVimeo && WVCVimeo.players[ VimeoPlayerId ] ) {
-						WVCVimeo.players[ VimeoPlayerId ].play();
+				} else if ($iframe.hasClass("wvc-vimeo-bg")) {
+					if (
+						"undefined" !== typeof WVCVimeo &&
+						WVCVimeo.players[VimeoPlayerId]
+					) {
+						WVCVimeo.players[VimeoPlayerId].play();
 					}
 				}
-
-			} else if ( $video.length ) {
-
-				$video.trigger( 'play' );
+			} else if ($video.length) {
+				$video.trigger("play");
 			}
-
 		},
 
 		/**
 		 * Pause all videos
 		 */
-		pauseAllVideos : function ( $container ) {
-
+		pauseAllVideos: function ($container) {
 			var _this = this,
 				VimeoPlayerId,
 				$video;
 
 			/* Pause all HTML videos */
-			if ( $container.find( '.wvc-video-bg' ).length ) {
-				$container.find( '.wvc-video-bg' ).each( function () {
-					$video = $( this );
+			if ($container.find(".wvc-video-bg").length) {
+				$container.find(".wvc-video-bg").each(function () {
+					$video = $(this);
 					$video.get(0).pause();
-				} );
+				});
 			}
 
 			/* Pause all YT videos */
-			$container.find( '.wvc-yt-video-bg-pause' ).trigger( 'click' );
+			$container.find(".wvc-yt-video-bg-pause").trigger("click");
 
 			/* Pause all Vimeo  videos */
-			$container.find( '.wvc-vimeo-video-bg-container > iframe' ).each( function () {
-				VimeoPlayerId = $( this ).data( 'vimeo-bg-element-id' );
-				if ( 'undefined' !== typeof WVCVimeo && WVCVimeo.players[ VimeoPlayerId ] ) {
-					WVCVimeo.players[ VimeoPlayerId ].pause();
-				}
-			} );
+			$container
+				.find(".wvc-vimeo-video-bg-container > iframe")
+				.each(function () {
+					VimeoPlayerId = $(this).data("vimeo-bg-element-id");
+					if (
+						"undefined" !== typeof WVCVimeo &&
+						WVCVimeo.players[VimeoPlayerId]
+					) {
+						WVCVimeo.players[VimeoPlayerId].pause();
+					}
+				});
 		},
 
 		/**
 		 * Delay anim n page load
 		 */
-		pageLoad : function() {
-			
-			setTimeout( function() {
-				WVC.delayWow( '.wvc-io-item-content' );
-				WVC.resetAOS( '.wvc-io-item-content' );
-			}, 1000 );
-		}
+		pageLoad: function () {
+			setTimeout(function () {
+				WVC.delayWow(".wvc-io-item-content");
+				WVC.resetAOS(".wvc-io-item-content");
+			}, 1000);
+		},
 	};
+})(jQuery);
 
-}( jQuery );
+(function ($) {
+	"use strict";
 
-( function( $ ) {
-
-	'use strict';
-
-	$( document ).ready( function() {
+	$(document).ready(function () {
 		WVCInteractiveOverlays.init();
-	} );
+	});
 
-	$( window ).load( function() {
+	$(window).load(function () {
 		WVCInteractiveOverlays.pageLoad();
-	} );
-
-} )( jQuery );
+	});
+})(jQuery);
